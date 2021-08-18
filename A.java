d513f6e6a4d7281830afadb7ae563e2ca938f21d
Chirag <achirag8k@gmail.com>
2021-08-18 16:16:35 +0530

    NA

--- A.java
+++ A.java
@@ -1,15 +1,69 @@
-public class A
-{
-     static int factorial(int n){    
-      if (n == 0)    
-        return 1;    
-      else   
-        return(n * factorial(n-1));    
-     }    
-     public static void main(String args[]){  
-      int i,fact=1;  
-      int number=5;    
-      fact = factorial(number);   
-      System.out.println("Factorial of "+number+" is = "+fact);    
-     }  
+class all {
+  int number;
+
+  all() {
+    number = 13;
+  }
+
+  int factorial(int n) {
+    if (n == 0)
+      return 1;
+    else
+      return (n * factorial(n - 1));
+  }
+
+  void prime(int n) {
+    int i, flag = 0, m = n / 2;
+    if (n == 0 || n == 1) {
+      System.out.println(n + " is not prime number");
+    } else {
+      for (i = 2; i <= m; i++) {
+        if (n % i == 0) {
+          System.out.println(n + " is not prime number");
+          flag = 1;
+          break;
+        }
+      }
+      if (flag == 0) {
+        System.out.println(n + " is prime number");
+      }
     }
+  }
+
+  void armstrong(int number) {
+    int originalNumber, remainder, result = 0, n = 0;
+    originalNumber = number;
+    for (; originalNumber != 0; originalNumber /= 10, ++n)
+      ;
+    originalNumber = number;
+    for (; originalNumber != 0; originalNumber /= 10) {
+      remainder = originalNumber % 10;
+      result += Math.pow(remainder, n);
+    }
+    if (result == number)
+      System.out.println(number + " is an Armstrong number.");
+    else
+      System.out.println(number + " is not an Armstrong number.");
+  }
+
+  void fibo() {
+    int n1 = 0, n2 = 1, n3, i, count = 10;
+    System.out.print(n1 + " " + n2);
+    for (i = 2; i < count; ++i) {
+      n3 = n1 + n2;
+      System.out.print(" " + n3);
+      n1 = n2;
+      n2 = n3;
+    }
+  }
+}
+
+public class A {
+  public static void main(String args[]) {
+    all ob = new all();
+    System.out.println("Factorial of " + ob.number + " is = " + ob.factorial(ob.number));
+    ob.prime(ob.number);
+    ob.armstrong(ob.number);
+    ob.fibo();
+  }
+}
--- test1.java
+++ test1.java
@@ -1,30 +1,27 @@
 import java.util.*;
-class test1
-{  
- public static void test1()
- {
-     int[] arr= new int[20];
-     Scanner sc= new Scanner(System.in);
-     for(int i=0;i<20;i++)
-     {
-       arr[i]= sc.nextInt(); 
-     }
-     int smallest= arr[0];
-     int largest = arr[0];
-     int sum=0;
-     for(int i=1;i< arr.length;i++)
-     {
-         if (arr[i] > largest)
-         largest= arr[i];
-         else if (arr[i] < smallest)
-         smallest = arr[i];
-     }
-     System.out.println("smallest no is="+smallest);
-     System.out.println(" largest no is="+ largest);
-     for (int i=0;i< 20;i++)
-     {
-         sum+= arr[i];
-         System.out.println("average is"+sum/20);
-     }
+
+class test1 {
+    public static void main() {
+        int[] arr = new int[20];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 20; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int smallest = arr[0];
+        int largest = arr[0];
+        int sum = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
+            else if (arr[i] < smallest)
+                smallest = arr[i];
+        }
+        System.out.println("smallest no is=" + smallest);
+        System.out.println(" largest no is=" + largest);
+        for (int i = 0; i < 20; i++) {
+            sum += arr[i];
+            System.out.println("average is" + sum / 20);
+        }
+        sc.close();
     }
 }
