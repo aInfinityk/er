27cd7d87d2ad6700e66f880818ec29968f396d20
Chirag <achirag8k@gmail.com>
2021-08-18 14:26:28 +0530

    first commit

---
+++ krish.java
@@ -0,0 +1,14 @@
+ class krish
+ {
+     public static void main(int a,int b)
+     {
+         int s,sub,mul;
+         s=a+b;
+         sub=a-b;
+         mul=a*b;
+         System.out.println("sum of nos ="+s);
+         System.out.println("subtraction of nos="+sub);
+         System.out.println("multiplication of nos="+mul);
+        }
+    }
+    
